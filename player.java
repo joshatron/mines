675c1f8e28326a9993d3e279628998bc8be37e9d
Joshua Leger <legermjoshua@gmail.com>
2013-04-30 01:04:50 -0700

    Made more progress on logging high scores.

--- player.java
+++ player.java
@@ -8,47 +8,45 @@ public class player
 {
 	String name;
 	int played1, played2, played3, won, best1, best2, best3, favorite;
+	int[] played, best;
 
 	public player()
 	{
 		name = "";
-		played1 = played2 = played3 = won = favorite = 0;
-		best1 = best2 = best3 = 999;
+		won = favorite = 0;
+		played = {0, 0, 0};
+		best = {999, 999, 999};
 	}
 	
 	public player(String n, int p1, int p2, int p3, int w, int b1, int b2, int b3, int f)
 	{
 		name = n;
-		played1 = p1;
-		played2 = p2;
-		played3 = p3;
 		won = w;
-		best1 = b1;
-		best2 = b2;
-		best3 = b3;
 		favorite = f;
+		played = {p1, p2, p3};
+		best = {b1, b2, b3};
 	}
 
 	public void updateFavorite()
 	{
-		if(played1 > played2)
+		if(played[0] > played[1])
 		{
-			if(played1 > played3)
+			if(played[0]> played[2])
 			{
 				favorite = 1;
 			}
-			else if(played3 > played1)
+			else if(played[2] > played[0])
 			{
 				favorite = 3;
 			}
 		}
 		else
 		{
-			if(played2 > played3)
+			if(played[1] > played[2])
 			{
 				favorite = 2;
 			}
-			else if(played3 > played2)
+			else if(played[2] > played[1])
 			{
 				favorite = 3;
 			}
@@ -57,14 +55,14 @@ public class player
 
 	public String getName(){return name;}
 	public int getPlayed(){return played1 + played2 + played3;}
-	public int getPlayed1(){return played1;}
-	public int getPlayed2(){return played2;}
-	public int getPlayed3(){return played3;}
+	public int getPlayed1(){return played[0];}
+	public int getPlayed2(){return played[1];}
+	public int getPlayed3(){return played3[2];}
 	public int getWon(){return won;}
 	public double getPercentWin(){return (double)won / (double)getPlayed() * 100.;}
-	public int getBest1(){return best1;}
-	public int getBest2(){return best2;}
-	public int getBest3(){return best3;}
+	public int getBest1(){return best[0];}
+	public int getBest2(){return best[1];}
+	public int getBest3(){return best[2];}
 	public int getFavorite(){return favorite;}
 
 	public String toString()
@@ -76,33 +74,25 @@ public class player
 		String str = new String();
 
 		str += name + ":\n\n";
-		str += "Game 1-\n\tGames Played: " + played1 + "\n\tFastest Time: " + best1 + "\n";
-		str += "Game 2-\n\tGames Played: " + played2 + "\n\tFastest Time: " + best2 + "\n";
-		str += "Game 3-\n\tGames Played: " + played3 + "\n\tFastest Time: " + best3 + "\n";
+		str += "Game 1-\n\tGames Played: " + played[0] + "\n\tFastest Time: " + best[0] + "\n";
+		str += "Game 2-\n\tGames Played: " + played[1] + "\n\tFastest Time: " + best[1] + "\n";
+		str += "Game 3-\n\tGames Played: " + played[2] + "\n\tFastest Time: " + best[2] + "\n";
 		str += "Totals:\n\tTotal Played: " + getPlayed() + "\n\tGames Won: " + won;
 
 		return str;
 	}
 
 	public void setName(String n){name = n;}
-	public void setWon(int w){won = w;}
-	public void setBest1(int b){best1 = b;}
-	public void setBest2(int b){best2 = b;}
-	public void setBest3(int b){best3 = b;}
+	public void setBest1(int g, int b){best[g - 1] = b;}
 	
-	public void incP1()
+	public void incP(int game)
 	{
-		played1++;
+		played[game - 1]++;
 		updateFavorite();
 	}
-	public void incP2()
+
+	public void incWon()
 	{
-		played2++;
-		updateFavorite();
-	}
-	public void incP3()
-	{
-		played3++;
-		updateFavorite();
+		won++;
 	}
 }
